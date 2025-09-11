d9ab35783441e792434fbf08ccaaea2af4e9318f
Calvin Kuven <calle@MacBook-Air-von-Calvin.local>
2025-09-11 17:44:49 +0200

    feat: generate dynamic HTML output for movie list with CSS styling

---
+++ write__html.py
@@ -0,0 +1,49 @@
+from storage.movie_storage_sql import get_movies
+import os
+
+
+
+def serialize_type():
+    """ returns the html for each movie """
+    movies = get_movies()
+
+    output = ""
+    output += '<html>\n'
+    output += '<head>\n'
+    output += '<title>My Movie App</title>\n'
+    output += '<link rel="stylesheet" href="style.css"/>\n'
+    output += '</head>\n'
+    output += '<body>\n'
+    output += '<div class="list-movies-title">\n'
+    output += '<h1>My Movie App</h1>\n'
+    output += '</div>\n'
+    output += '<div>\n'
+    output += '<ol class="movie-grid">\n'
+
+
+    for title, info in movies.items():
+        output += '<li>\n'
+        output += '<div class="movie">\n'
+        output += f'<img class="movie-poster" src="{info["poster"]}"/>\n'
+        output += f'<div class="movie-title">{title}</div>\n'
+        output += f'<div class="movie-year">{info["year"]}</div>\n'
+        output += '</div>\n'
+        output += '</li>\n'
+
+    output += '</ol>\n'
+    output += '</div>\n'
+    output += '</body>\n'
+    output += '</html>\n'
+
+    return output
+
+
+def write_html(output):
+    """ writes the html to disk """
+
+    output_path = os.path.join("web", "movies.html")
+    with open(output_path, "w") as file:
+        file.write(serialize_type())
+        print("Website was generated successfully.")
+
+
