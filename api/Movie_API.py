3f1f837702efb9c5a962d741d847636d538559a4
Calvin Kuven <calle@MacBook-Air-von-Calvin.local>
2025-09-14 13:50:58 +0200

    Refactor exception handling
    - Moved API exception handling from main.py into api/Movie_API.py.
    - Centralizes error handling closer to the API logic for better structure.
    - Keeps main.py cleaner and focused on program flow.

--- api/Movie_API.py
+++ api/Movie_API.py
@@ -1,6 +1,7 @@
 import requests
 import os
 from dotenv import load_dotenv
+from utils.Fore_color import fore_color_text, Fore
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
@@ -12,7 +13,19 @@ def search_movie_and_get_movies(search_movie):
     :param search_movie: user-input search movie name
     :return: returns a list of dictionaries containing movie details
     """
-    url = f"http://www.omdbapi.com/?apikey={API_KEY}&t={search_movie}"
-    response = requests.get(url)
-    movies_as_json = response.json()
-    return movies_as_json
+    try:
+        url = f"http://www.omdbapi.com/?apikey={API_KEY}&t={search_movie}"
+        response = requests.get(url)
+        movies_as_json = response.json()
+        return movies_as_json
+    except KeyError:
+        print("Key not found.")
+    except requests.exceptions.ConnectionError:
+        print("No connection to the API (ConnectionError).")
+    except requests.exceptions.Timeout:
+        print("API request took too long (Timeout).")
+    except requests.exceptions.HTTPError as e:
+        print(f"HTTP-Error: {e.response.status_code}")
+
+    user_input = input(fore_color_text("\nPress Enter to continue...", Fore.YELLOW))
+
